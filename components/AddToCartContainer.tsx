23c34fb75ca60833c72c7c6c987e70ae47b695ba
Loki343 <kausikbhaumik42@gmail.com>
2024-08-26 13:58:55 +0530

    2hr10min

---
+++ components/AddToCartContainer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddToCartContainer = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AddToCartContainer
--- lib/supabase/hooks/useSupabase.ts
+++ lib/supabase/hooks/useSupabase.ts
@@ -25,7 +25,7 @@ export const useSupabase = () => {
         }
     };
     //for get single product
-    const [singleProduct, setSingleProduct] = useState<any>({});
+    const [singleProduct, setSingleProduct] = useState<any>([]);
     const getSingleProduct = async (id: number) => {
         let { data, error } = await supabase.from("products").select("*").eq('id', id)
         if (data) {
